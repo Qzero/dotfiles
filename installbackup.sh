3500a21ff58b7750c8b90c169198e2f0fadceb44
Chieh Ho <chieh.ho@hotmail.com>
2025-03-06 21:31:57 +0800

    恢复脚本更新

--- installbackup.sh
+++ installbackup.sh
@@ -1,10 +1,11 @@
 #Mac重装系统后恢复安装软件脚本
 #!/bin/bash
 
-# 设置变量
-DOTFILES_DIR="$HOME/dotfiles"
+# 1 基本设置
+DOTFILES_DIR="$HOME/dotfiles" # dotfiles 的路径
+BREWFILE_PATH="$DOTFILES_DIR/dotfiles/.brewfile"  # Brewfile 的路径
 
-# 安装 Homebrew（如果尚未安装）
+# 2 判断安装 Homebrew
 if ! command -v brew &> /dev/null; then
     echo "安装 Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -14,7 +15,11 @@ if ! command -v brew &> /dev/null; then
 else
     echo "Homebrew 已安装。"
 fi
+# 2.1 通过brewfile恢复安装程序
+brew install mas # 命令行安装 app store工具
+brew bundle --file="~/dotfiles/.brewfile"
 
+# 3 dotfiles文件夹设置配置
 # 检查 dotfiles 目录是否存在
 if [ -d "$DOTFILES_DIR" ]; then
     echo "找到 dotfiles 目录，开始配置..."
@@ -22,22 +27,18 @@ if [ -d "$DOTFILES_DIR" ]; then
 
     # 安装 dotfiles 中的配置
     if [ -f "installbackup.sh" ]; then
-        echo "运行 dotfiles 安装脚本..."
-        ./install.sh
+        echo "运行 dotfiles 恢复脚本..."
+        ./installbackup.sh
     else
-        echo "未找到 installbackup.sh 脚本，手动创建符号链接..."
-        # 手动创建符号链接
-        ln -sf "$DOTFILES_DIR/.vimrc" ~/.vimrc  # 添加 .vimrc 软连接
-        ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
-        # 添加更多配置文件...
+        echo "未找到 installbackup.sh 脚本"
     fi
 else
     echo "未找到 dotfiles 目录，跳过配置。"
 fi
 
-# 通过brewfile恢复安装程序
-brew install mas
-brew bundle --file="~/dotfiles/.brewfile"
+# 4,软链接配置文件
+ln -sf "$DOTFILES_DIR/.vimrc" ~/.vimrc  # 添加 .vimrc 软连接
+ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf # 添加 .tmux.conf 软连接
 
 # 设置 macOS 系统偏好（可选）
 echo "设置 macOS 系统偏好..."
